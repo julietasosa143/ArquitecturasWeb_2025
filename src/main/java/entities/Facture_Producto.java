d1b30a81b15fa13103637ee6d310f22b045ba020
Cristian <cris.andres.gz@gmail.com>
2025-09-02 17:32:43 -0300

    agrega entity Factura_Producto

---
+++ src/main/java/entities/Facture_Producto.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Facture_Producto {
+    private Integer idFactura;
+    private Integer IdProducto;
+    private Integer cantidad;
+
+    public Facture_Producto(Integer idFactura, Integer IdProducto, Integer cantidad) {
+        this.idFactura = idFactura;
+        this.IdProducto = IdProducto;
+        this.cantidad = cantidad;
+    }
+
+    public Facture_Producto() {}
+
+    public Integer getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Integer getIdProducto() {
+        return IdProducto;
+    }
+
+    public void setIdProducto(Integer IdProducto) {
+        this.IdProducto = IdProducto;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String toString() {
+        return "Factura_Producto{" +
+                ", idFactura" + idFactura +
+                ", IdProducto" + IdProducto +
+                ", cantidad" + cantidad +
+                "}";
+    }
+}
