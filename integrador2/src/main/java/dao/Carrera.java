f6186c7800d4e7b95c090ee5c1f4a5085725e88c
miriamgonzalez23 <miriamgonzalez121@gmail.com>
2025-10-01 15:55:48 -0300

    agrego entidad carrera

--- integrador2/src/main/java/dao/Carrera.java
+++ integrador2/src/main/java/dao/Carrera.java
@@ -1,4 +1,34 @@
 package dao;
-
+@Entity
 public class Carrera {
+    @Id
+    private int idCarrera;
+    private String nombreCarrera;
+
+    @OneToMany(mappedBy = "carrera")
+    private List<Inscripcion> inscripciones = new ArrayList<>();
+
+    public Carrera() {
+        this.idCarrera = idCarrera;
+        this.nombreCarrera = nombreCarrera;
+    }
+
+    public String getNombreCarrera() {
+        return nombreCarrera;
+    }
+
+    public void setNombreCarrera(String nombreCarrera) {
+        this.nombreCarrera = nombreCarrera;
+    }
+
+    public List<Inscripcion> getInscripciones() {
+        return inscripciones;
+
+    }
+
+    public void setInscripciones(List<Inscripcion> inscripciones) {
+        this.inscripciones = inscripciones;
+    }
+
+
 }
