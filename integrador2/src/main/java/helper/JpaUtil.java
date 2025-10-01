aea9e3bc915b5d370f30be10671af12a26bb1a38
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2025-10-01 17:15:29 -0300

    agrega jpautil

---
+++ integrador2/src/main/java/helper/JpaUtil.java
@@ -0,0 +1,19 @@
+package helper;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+
+        private static final EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("Example");
+
+        public static EntityManager getEntityManager() {
+            return emf.createEntityManager();
+        }
+
+        public static void close() {
+            emf.close();
+        }
+}
