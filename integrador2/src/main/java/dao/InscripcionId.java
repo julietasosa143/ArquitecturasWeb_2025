1eac9402dd1d19a55842557a762dcf117e2bb8e9
Cristian <cris.andres.gz@gmail.com>
2025-10-01 16:34:08 -0300

    agrega inscripcion e inscripcionId

--- integrador2/src/main/java/dao/InscripcionId.java
+++ integrador2/src/main/java/dao/InscripcionId.java
@@ -1,10 +1,31 @@
 package dao;
 
+import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 import javax.persistence.EmbeddedId;
-
-public class InscripcionId {
+import java.io.Serializable;
+@Embeddable
+public class InscripcionId implements Serializable {
     private int idCarrera;
-    private int idEstudiante;
+    private int dniEstudiante;
 
+    public InscripcionId() {}
+    public InscripcionId(int idCarrera, int idEstudiante) {
+        this.idCarrera = idCarrera;
+        this.dniEstudiante = idEstudiante;
+    }
+
+    public int getIdCarrera() {
+        return idCarrera;
+    }
+    public void setIdCarrera(int idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+    public int getDniEstudiante() {
+        return dniEstudiante;
+    }
+    public void setDniEstudiante(int idEstudiante) {
+        this.dniEstudiante = idEstudiante;
+    }
 }
+
