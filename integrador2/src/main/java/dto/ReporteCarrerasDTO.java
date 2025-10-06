54359aab4dc3c65d762402ed087225589cf75c90
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2025-10-06 19:00:03 -0300

    agrega punto 3

--- integrador2/src/main/java/dto/ReporteCarrerasDTO.java
+++ integrador2/src/main/java/dto/ReporteCarrerasDTO.java
@@ -1,6 +1,31 @@
 package dto;
 
-public class ReporteCarreras {
+public class ReporteCarrerasDTO {
     private String nombreCarrera;
-    
+    private int anioInscripcion;
+    private int cantidadInscriptos;
+    private int cantidadEgresados;
+
+    public ReporteCarrerasDTO(String nombreCarrera,int anioInscripcion, int cantidadInscriptos,int cantidadEgresados ) {
+        this.nombreCarrera = nombreCarrera;
+        this.anioInscripcion = anioInscripcion;
+        this.cantidadInscriptos = cantidadInscriptos;
+        this.cantidadEgresados = cantidadEgresados;
+    }
+
+    public String getNombreCarrera() {
+        return nombreCarrera;
+    }
+
+    public int getAnioInscripcion() {
+        return anioInscripcion;
+    }
+
+    public int getCantidadInscriptos() {
+        return cantidadInscriptos;
+    }
+
+    public int getCantidadEgresados() {
+        return cantidadEgresados;
+    }
 }
