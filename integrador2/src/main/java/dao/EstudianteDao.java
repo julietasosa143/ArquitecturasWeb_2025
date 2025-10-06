c106b4a8a8ad0f169ad316dc388be7e77b3aae86
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2025-10-06 18:24:16 -0300

    agrega punto B

--- integrador2/src/main/java/dao/EstudianteDao.java
+++ integrador2/src/main/java/dao/EstudianteDao.java
@@ -15,7 +15,7 @@ public class EstudianteDao {
     public EstudianteDao() {
         this.em = JpaUtil.getEntityManager();
     }
-
+//A) dar de alta un estudiante
     public void darDeAltaEstudiante(Estudiante estudiante) {
         try{em.getTransaction().begin();
         em.persist(estudiante);
@@ -25,7 +25,7 @@ public class EstudianteDao {
         }
 
     }
-
+// C) recuperar todos los estudiantes, y especificar algún criterio de ordenamiento simple. Elegimos libreta universitaria
     public void getAllEstudiantesByLU() {
         try{em.getTransaction().begin();
             String spql = "SELECT e FROM Estudiante e ORDER BY e.luEstudiante";
@@ -39,7 +39,7 @@ public class EstudianteDao {
 
 
     }
-
+    // D)recuperar un estudiante, en base a su número de libreta universitaria.
     public void findEstudianteByLU(int luEstudiante){
         try{em.getTransaction().begin();
             String spql= "Select e from Estudiante e WHERE e.luEstudiante =:luEstudiante ";
@@ -52,6 +52,7 @@ public class EstudianteDao {
 
         }finally{em.close();}
     }
+    //E) recuperar todos los estudiantes, en base a su género.
     public void findEstudianteByGender(String gender){
         try{em.getTransaction().begin();
             String spql= "Select e from Estudiante e WHERE e.generoEstudiante =:gender ";
