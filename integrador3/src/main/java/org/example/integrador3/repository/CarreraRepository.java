ead32884674c5a27be16dbec575c3d450e0ac944
miriamgonzalez23 <miriamgonzalez121@gmail.com>
2025-10-17 17:19:58 -0300

    agrega CarreraRepository dto y demas

--- integrador3/src/main/java/org/example/integrador3/repository/CarreraRepository.java
+++ integrador3/src/main/java/org/example/integrador3/repository/CarreraRepository.java
@@ -1,4 +1,19 @@
 package org.example.integrador3.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.example.integrador3.dto.response.CarreraInscriptosResponseDTO;
+import org.example.integrador3.model.Carrera;
 
-public class CarreraRepository {
+import java.util.List;
+
+@Repository
+public interface CarreraRepository extends JpaRepository<Carrera, Long>{
+    //Recupera las carreras con estudiantes inscriptos, ordenadas por cantidad de inscriptos
+    @Query("SELECT new org.example.integrador3.dto.response.CarreraInscriptosResponseDTO(c.nombre, COUNT(e)) " +
+    "FROM Carrera c " +
+    "JOIN c.estudiantes e " +
+    "GROUP BY c.nombre " +
+    "ORDER BY COUNT(e) DESC")
+    List<CarreraInscriptosResponseDTO> findCarreraConInscriptosOrdenadas();
 }
