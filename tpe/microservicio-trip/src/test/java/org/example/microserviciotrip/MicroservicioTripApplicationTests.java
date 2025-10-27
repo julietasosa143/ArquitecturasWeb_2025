9c631fa1ae4e7c538f48a5b7a16c3f44c6fa2200
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2025-10-27 17:31:29 -0300

    agrega estructura inicial tpe

---
+++ tpe/microservicio-trip/src/test/java/org/example/microserviciotrip/MicroservicioTripApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microserviciotrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ tpe/microservicio-user/src/main/java/org/example/microserviciouser/MicroservicioUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.microserviciouser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioUserApplication.class, args);
+    }
+
+}
