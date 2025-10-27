945180b6c46ddaff04a675e0d14a49c1933b4e3b
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2025-10-27 19:18:28 -0300

    avance del microservicio-user

---
+++ tpe/microservicio-user/src/main/java/controller/UsuarioController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import entities.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.UsuarioService;
+
+import java.util.List;
+import java.util.ListResourceBundle;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+
+    @GetMapping ("/")
+    public ResponseEntity<List<Usuario>> getAll(){
+        List<Usuario> users = usuarioService.getAll();
+        if(users.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        }
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id){
+        Usuario usuario = usuarioService.getById(id);
+        if(usuario == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else  {
+            return new ResponseEntity<>(usuario, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Usuario> save(@RequestBody Usuario user) {
+        Usuario nuevo = usuarioService.save(user);
+        return ResponseEntity.ok(nuevo);
+    }
+
+}
---
+++ tpe/microservicio-user/src/main/java/service/UsuarioService.java
@@ -0,0 +1,33 @@
+package service;
+
+
+import entities.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import repository.UsuarioRepository;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public List<Usuario> getAll(){
+        return usuarioRepository.findAll();
+    }
+    public Usuario getById(Long id){
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public Usuario save(Usuario usuario){
+        Usuario nuevo= usuarioRepository.save(usuario);
+        return nuevo;
+    }
+
+}
