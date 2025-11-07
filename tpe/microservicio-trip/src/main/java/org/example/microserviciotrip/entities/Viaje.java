aaeab15c9c7b39cc0a3e99b971eff1b84ce20ca5
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2025-11-07 18:28:55 -0300

    no anda nada no juego mas

--- tpe/microservicio-trip/src/main/java/org/example/microserviciotrip/entities/Viaje.java
+++ tpe/microservicio-trip/src/main/java/org/example/microserviciotrip/entities/Viaje.java
@@ -3,7 +3,9 @@ package org.example.microserviciotrip.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,17 +23,21 @@ public class Viaje {
     @OneToMany( mappedBy = "viaje",cascade = CascadeType.ALL, orphanRemoval = true )
     private List<Pausa> pausas;
     @Column
-    private float kilometros;
+    private double kilometros;
     @Column
-    private float tiempo;
+    private double tiempo;
     @Column
-    private float tarifa;
+    private double tarifa;
     @Column
     private Integer idMonopatin;
     @Column
     private Integer idUsuario;
+    @Column
+    private LocalDate fechaInicio;
+    @Column
+    private LocalDate fechaFin;
 
-    public Viaje(Integer id  ,Integer paradaInicio,Integer paradaFin, float kilometros, float tiempo, float tarifa, Integer monopatin, Integer usuario){
+    public Viaje(Integer id  ,Integer paradaInicio,Integer paradaFin, double kilometros, double tiempo, double tarifa, Integer monopatin, Integer usuario,  LocalDate fechaInicio, LocalDate fechaFin) {
         this.id = id;
         this.idParadaInicio = paradaInicio;
         this.idParadaFin = paradaFin;
@@ -41,10 +47,12 @@ public class Viaje {
         this.tarifa = tarifa;
         this.idMonopatin = monopatin;
         this.idUsuario = usuario;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
     }
 
 
-    public  Viaje( Integer id ,Integer pInicio, Integer pFin, float tiempo, Integer monopatin, Integer usuario){
+    public  Viaje(Integer id , Integer pInicio, Integer pFin, double tiempo, Integer monopatin, Integer usuario, LocalDate fechaInicio){
         this.id = id;
         this.idParadaInicio = pInicio;
         this.idParadaFin = pFin;
@@ -52,6 +60,7 @@ public class Viaje {
         this.idMonopatin = monopatin;
         this.idUsuario = usuario;
         this.pausas= new ArrayList<Pausa>();
+        this.fechaInicio = fechaInicio;
     }
 
 
