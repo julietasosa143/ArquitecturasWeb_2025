c86ffce5412e8bc6a5960882f5907198c63c339f
julietasosa143 <jsosa@alumnos.exa.unicen.edu.ar>
2025-11-12 15:47:19 -0300

    empieza inciso f

---
+++ tpe/microservicio-user/src/main/java/org/example/microserviciouser/dto/ParadaResponseDTO.java
@@ -0,0 +1,16 @@
+package org.example.microserviciouser.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParadaResponseDTO {
+    long idParada;
+    int x;
+    int y;
+
+}
+
